1db36ba4284692fa0c927a770d92df84ad24a427
Pete Bevin <pete@hover.com>
2014-06-19 09:02:05 -0400

    Apply primitive functions

--- main.hs
+++ main.hs
@@ -98,8 +98,31 @@ eval val@(String _) = val
 eval val@(Number _) = val
 eval val@(Bool _) = val
 eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args)) = apply func args
 
 
+
+apply :: String -> [LispVal] -> LispVal
+apply fn args = maybe (Bool False) ($ args) $ lookup fn primitives
+
+primitives :: [ (String, [LispVal] -> LispVal) ]
+primitives = [
+   ( "+", numericBinop (+) ),
+   ( "-", numericBinop (-) ),
+   ( "*", numericBinop (*) ),
+   ( "/", numericBinop (div) ),
+   ( "mod", numericBinop mod ),
+   ( "quotient", numericBinop quot ),
+   ( "remainder", numericBinop rem )
+
+   ]
+
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op args = Number $ foldl1 op $ map unpackNum args
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number a) = a
 
 readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of

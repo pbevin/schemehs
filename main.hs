e1fe77d231d4e4480a7b35cf1c3481fc4c3a9db5
Pete Bevin <pete@petebevin.com>
2014-06-22 13:59:32 -0400

    (define) function

--- main.hs
+++ main.hs
@@ -16,6 +16,8 @@ data LispVal = Atom String
              | Number Integer
              | String String
              | Bool Bool
+             | PrimitiveFunc ([LispVal] -> ThrowsError LispVal)
+             | Func { params :: [String], vararg :: (Maybe String), body :: [LispVal], closure :: Env }
 
 data LispError = NumArgs Integer [LispVal]
                | TypeMismatch String LispVal
@@ -120,10 +122,22 @@ showVal (Number n) = show n
 showVal (String str) = "\"" ++ str ++ "\""
 showVal (Bool True) = "#t"
 showVal (Bool False) = "#f"
+showVal (PrimitiveFunc _) = "<primitive>"
+showVal (Func { params = args, vararg = varargs, body = body, closure = env}) =
+  "(lambda (" ++ unwords (map show args) ++
+    (case varargs of
+      Nothing -> ""
+      Just arg -> " . " ++ arg) ++ ") ...)"
+
+
 unwordsList :: [LispVal] -> String
 unwordsList = unwords . map showVal
 
 instance Show LispVal where show = showVal
+
+makeFunc varargs env params body = return $ Func (map showVal params) varargs body env
+makeNormalFunc = makeFunc Nothing
+makeVarArgs = makeFunc . Just . showVal
 
 eval :: Env -> LispVal -> IOThrowsError LispVal
 eval env val@(String _) = return val
@@ -138,13 +152,37 @@ eval env (List [Atom "if", pred, conseq, alt]) =
        _ -> eval env conseq
 eval env (List [Atom "set!", Atom var, form]) = eval env form >>= setVar env var
 eval env (List [Atom "define", Atom var, form]) = eval env form >>= defineVar env var
-eval env (List (Atom func : args)) = mapM (eval env) args >>= liftThrows . apply func
+eval env (List (Atom "define" : List (Atom var : params) : body)) =
+  makeNormalFunc env params body >>= defineVar env var
+eval env (List (Atom "define" : DottedList (Atom var : params) varargs : body)) =
+  makeVarArgs varargs env params body >>= defineVar env var
+eval env (List (Atom "lambda" : List params : body)) =
+  makeNormalFunc env params body
+eval env (List (Atom "lambda" : DottedList params varargs : body)) =
+  makeVarArgs varargs env params body
+eval env (List (Atom "lambda" : varargs@(Atom _) : body)) =
+  makeVarArgs varargs env [] body
+-- eval env (List (Atom func : args)) = mapM (eval env) args >>= liftThrows . apply func
+eval env (List (function : args)) = do
+  func <- eval env function
+  argVals <- mapM (eval env) args
+  apply func argVals
+
 eval env badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
 
+apply :: LispVal -> [LispVal] -> IOThrowsError LispVal
+apply (PrimitiveFunc func) args = liftThrows $ func args
+apply (Func params varargs body closure) args =
+  if num params /= num args && varargs == Nothing
+    then throwError $ NumArgs (num params) args
+    else (liftIO $ bindVars closure $ zip params args) >>= bindVarArgs varargs >>= evalBody
+  where remainingArgs = drop (length params) args
+        num = toInteger . length
+        evalBody env = liftM last $ mapM (eval env) body
+        bindVarArgs arg env = case arg of
+          Just argName -> liftIO $ bindVars env [(argName, List $ remainingArgs)]
+          Nothing -> return env
 
-
-apply :: String -> [LispVal] -> ThrowsError LispVal
-apply fn args = maybe (throwError $ NotFunction "Unrecognized primitive function" fn) ($ args) $ lookup fn primitives
 
 primitives :: [ (String, [LispVal] -> ThrowsError LispVal) ]
 primitives = [
@@ -354,11 +392,15 @@ until_ pred prompt action = do
     then return ()
     else action result >> until_ pred prompt action
 
+primitiveBindings :: IO (Env)
+primitiveBindings = nullEnv >>= (flip bindVars $ map makePrimitiveFunction primitives)
+  where makePrimitiveFunction (var, func) = (var, PrimitiveFunc func)
+
 runOne :: String -> IO ()
-runOne expr = nullEnv >>= flip evalAndPrint expr
+runOne expr = primitiveBindings >>= flip evalAndPrint expr
 
 runRepl :: IO ()
-runRepl = nullEnv >>= until_ (== "quit") (readPrompt "λ>>> ") . evalAndPrint
+runRepl = primitiveBindings >>= until_ (== "quit") (readPrompt "λ>>> ") . evalAndPrint
 
 main :: IO ()
 main = do args <- getArgs
